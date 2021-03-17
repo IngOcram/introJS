f7e102d0f84fb53496ac10e136aa1d69f842e8fb
IngOcram <marco.flores85@hotmail.com>
2021-03-16 21:02:53 -0600

    variable let y const JS

---
+++ js/02.js
@@ -0,0 +1,23 @@
+// variables con let
+
+let producto = 'Audifonos Gamer' // Iniciar Variable y asignarle un valor
+
+let disponible //Iniciamos la variable pero sin valor
+
+// producto = true // Reasignando el valor de la variable
+
+disponible = true
+
+let producto1 = 'computadora',
+    disponible1 = true,
+    categoria = 'computadoras';
+
+// var 1disponible;
+
+let disponible_;
+
+//Estilos para las variables 
+let nombre_producto = 'monitor HD' //underscore
+let nombreProducto = 'monitor HD' // Camelcase
+let NombreProducto = 'Monitor HD' //Pascal Case
+let nombreproducto = 'MonitorHD'
---
+++ js/03.js
@@ -0,0 +1,23 @@
+// variables con let
+
+const producto = 'Audifonos Gamer' // Iniciar Variable y asignarle un valor
+
+const disponible = true //Iniciamos la variable pero sin valor
+
+// producto = true // Reasignando el valor de la variable
+
+// disponible = true
+
+const producto1 = 'computadora',
+    disponible1 = true,
+    categoria = 'computadoras';
+
+// var 1disponible;
+
+const disponible_ = true
+
+//Estilos para las variables 
+const nombre_producto = 'monitor HD' //underscore
+const nombreProducto = 'monitor HD' // Camelcase
+const NombreProducto = 'Monitor HD' //Pascal Case
+const nombreproducto = 'MonitorHD'
