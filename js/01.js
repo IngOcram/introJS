c6ee6114160ace2a2a81d4e56f44efa6d9d57909
IngOcram <marco.flores85@hotmail.com>
2021-03-16 20:23:20 -0600

    variables JS

---
+++ js/01.js
@@ -0,0 +1,25 @@
+// Variables
+
+var producto = 'Audifonos Gamer' // Iniciar Variable y asignarle un valor
+
+var disponible //Iniciamos la variable pero sin valor
+
+// producto = true // Reasignando el valor de la variable
+
+disponible = true
+
+var producto1 = 'computadora',
+    disponible1 = true,
+    categoria = 'computadoras';
+
+// var 1disponible;
+
+var disponible_;
+
+//Estilos para las variables 
+var nombre_producto = 'monitor HD' //underscore
+var nombreProducto = 'monitor HD' // Camelcase
+var NombreProducto = 'Monitor HD' //Pascal Case
+var nombreproducto = 'MonitorHD'
+
+console.log(producto)
