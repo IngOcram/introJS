4e1238c3bd703d647abb26e83cbf8aaa300f075f
IngOcram <marco.flores85@hotmail.com>
2021-03-22 20:43:55 -0600

    Metodos para los strings

--- js/04.js
+++ js/04.js
@@ -1,5 +1,17 @@
 // String o cadenas de texto
-const producto = "Monitor de 20\"";
+const tweet = "Aprendiendo Javascript con el curso web completo";
+const producto2 = "Monitor HD";
 
+//length es para la extension
+console.log(tweet.length);
+console.log(producto2);
 
-console.log(producto)
+//IndexOf (Retorna la posicion)
+
+console.log(tweet.indexOf('Javascript'));
+console.log(producto2.indexOf('Tablet'));
+
+//Includes (Retorna true o false)
+
+console.log(tweet.includes('Javascript'));
+console.log(producto2.includes('Tablet'));
