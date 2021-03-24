04a8cd532b37228f1dc8ec2f1b56800823bf3c89
IngOcram <marco.flores85@hotmail.com>
2021-03-23 21:45:00 -0600

    objeto Math en JS

---
+++ js/06.js
@@ -0,0 +1,16 @@
+// objeto math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.8);
+resultado = Math.ceil(2.3); // ciel siempre redondea hacia arriba
+resultado = Math.floor(2.9); // floor siempre redondea hacia abajo
+resultado = Math.sqrt(144);
+resultado = Math.abs(-100);
+resultado = Math.min(3, 5, 1, 10, 8, 20);
+resultado = Math.max(3, 5, 1, 10, 8, 20);
+resultado = Math.random();
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
