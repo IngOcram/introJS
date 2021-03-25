d532310d7da78051655b498a04467f66a65fc32b
IngOcram <marco.flores85@hotmail.com>
2021-03-24 19:53:49 -0600

    modificar objetos

--- js/10.js
+++ js/10.js
@@ -6,10 +6,21 @@ const producto = {
     disponible: true,
 }
 
+
+
+//console.log(producto.precio);
+//console.log(producto.nombreProducto);
+//console.log(producto.disponible);
+
+//console.log(producto["precio"]);
+
+
+//Agregar nuevas propiedades
+
+producto.imagen = "imagen.jpg";
+
+//eliminar propiedades
+
+delete producto.disponible;
+
 console.log(producto);
-
-console.log(producto.precio);
-console.log(producto.nombreProducto);
-console.log(producto.disponible);
-
-console.log(producto["precio"]);
