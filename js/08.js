537f95b6e16a43adf106a57b78241b071607d0cc
IngOcram <marco.flores85@hotmail.com>
2021-03-24 19:29:07 -0600

    concatenacion y template strings

---
+++ js/08.js
@@ -0,0 +1,11 @@
+const nombre = 'Marco Flores';
+const email = 'correo@correo.com'
+
+
+//Concatenacion
+
+console.log("Nombre Cliente: " + nombre + " email cliente: " + email);
+
+//Tamplete Strings - Strings Literal
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`)
